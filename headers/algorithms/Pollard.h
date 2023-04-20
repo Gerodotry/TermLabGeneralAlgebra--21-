882bbfab677be1a73f6917a57a36b66bec2c24b9
Radchenko Mukola <RadchenkoMukola@gmail.com>
2023-04-20 16:44:25 +0300

    need Unit tests

---
+++ headers/algorithms/Pollard.h
@@ -0,0 +1,18 @@
+﻿#ifndef POLLARD_H
+#define POLLARD_H
+
+#include <vector>
+#include <map>
+
+class  PollardFactorization
+{
+public:
+    /**
+     * @brief factorize use the Rho-Pollard's algo.
+     * @param number a number to take prime numbers of.
+     * @return a vector of prime numbers
+     */
+    static std::map<long long, int> factorize(long long number);
+};
+
+#endif // POLLARDFACTORIZATION_H
--- main.cpp
+++ main.cpp
@@ -7,4 +7,20 @@ int main() {
     Vector3D b{1, 2, 3};
     std::cout << VectorMultiplication::run(a, b) << std::endl;
     std::cout << "Hello world" << std::endl;
+    
+    std::map<long long, int> pn1 = PollardFactorization::factorize(10403);
+    std::map<long long, int> ans1{std::pair(7, 1), std::pair(13, 1)};
+
+    for(map<long long,int>::const_iterator it = pn1.begin();
+        it != pn1.end(); ++it)
+    {
+        std::cout << it->first << " " << it->second;
+        cout <<endl;
+    }
+    for(map<long long,int>::const_iterator it = ans1.begin();
+        it != ans1.end(); ++it)
+    {
+        std::cout << it->first << " " << it->second;
+        cout <<endl;
+    }
 }
