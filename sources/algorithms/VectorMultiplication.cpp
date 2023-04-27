74f4d56a24c826e44a3b2759c0df4c24294eb120
Gerodotry <60816964+Gerodotry@users.noreply.github.com>
2023-04-27 23:07:59 +0300

    Delete VectorMultiplication.cpp

--- sources/algorithms/VectorMultiplication.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Dklishch on 3/7/2023.
-//
-
-#include "algorithms/VectorMultiplication.h"
-
-float VectorMultiplication::run(const Vector3D &vectorA, Vector3D &vectorB) {
-    return vectorA.x * vectorB.x + vectorA.y * vectorB.y + vectorA.z * vectorB.z;
-}
