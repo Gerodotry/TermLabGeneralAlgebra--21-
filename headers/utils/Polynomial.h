b4b6a0d249b6870b368c341be3e0ffdba4e7bad1
RaynGoslenko <olkhovatyi@knu.ua>
2023-04-27 20:58:33 +0300

    Created class to represent ring polynomial

---
+++ headers/utils/Polynomial.h
@@ -0,0 +1,36 @@
+#ifndef LAB_POLYNOMIAL_H
+#define LAB_POLYNOMIAL_H
+
+#include <vector>
+#include "PolynomialTerm.h"
+
+class Polynomial {
+    friend class PolynomialTerm;
+
+public:
+    // TODO: Maybe, it is better to use templates for constructors
+    Polynomial() = default;
+
+    explicit Polynomial(const std::vector<PolynomialTerm>& terms);
+
+    Polynomial(std::vector<Number> degrees, std::vector<Number> coefficients);
+
+    Polynomial(std::vector<int> degrees, std::vector<int> coefficients);
+
+    Polynomial(std::vector<unsigned int> degrees, std::vector<unsigned int> coefficients);
+
+    Polynomial(std::vector<std::string> degrees, std::vector<std::string> coefficients);
+
+    std::string toString();
+
+private:
+    void dropZeroes();
+
+protected:
+    std::vector<PolynomialTerm> terms;
+
+    void simplify(unsigned int modulo);
+};
+
+
+#endif //LAB_POLYNOMIAL_H
---
+++ sources/utils/Polynomial.cpp
@@ -0,0 +1,65 @@
+#include "utils/Polynomial.h"
+
+#include <utility>
+
+void Polynomial::dropZeroes() {
+    for (auto it = terms.begin(); it != terms.end(); ) {
+        if (it->isZero()) {
+            it = terms.erase(it);
+        } else {
+            ++it;
+        }
+    }
+}
+
+void Polynomial::simplify(unsigned int modulo) {
+    dropZeroes();
+    for (auto term : terms) {
+        term.simplify(modulo);
+    }
+}
+
+std::string Polynomial::toString() {
+    std::string result;
+    for (const auto& term : terms) {
+        std::string termString = term.toString();
+        result += (termString[0] == '-') ? termString : ("+" + termString);
+    }
+    return result.substr(1);
+}
+
+Polynomial::Polynomial(const std::vector<PolynomialTerm>& terms) {
+    this->terms = terms;
+}
+
+Polynomial::Polynomial(std::vector<Number> degrees, std::vector<Number> coefficients) {
+    if (degrees.size() == coefficients.size()) {
+        for (int i = 0; i < degrees.size(); i++) {
+            this->terms[i] = PolynomialTerm(degrees[i], coefficients[i]);
+        }
+    }
+}
+
+Polynomial::Polynomial(std::vector<int> degrees, std::vector<int> coefficients) {
+    if (degrees.size() == coefficients.size()) {
+        for (int i = 0; i < degrees.size(); i++) {
+            this->terms[i] = PolynomialTerm(degrees[i], coefficients[i]);
+        }
+    }
+}
+
+Polynomial::Polynomial(std::vector<unsigned int> degrees, std::vector<unsigned int> coefficients) {
+    if (degrees.size() == coefficients.size()) {
+        for (int i = 0; i < degrees.size(); i++) {
+            this->terms[i] = PolynomialTerm(degrees[i], coefficients[i]);
+        }
+    }
+}
+
+Polynomial::Polynomial(std::vector<std::string> degrees, std::vector<std::string> coefficients) {
+    if (degrees.size() == coefficients.size()) {
+        for (int i = 0; i < degrees.size(); i++) {
+            this->terms[i] = PolynomialTerm(degrees[i], coefficients[i]);
+        }
+    }
+}
