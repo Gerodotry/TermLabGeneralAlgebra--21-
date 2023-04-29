7300794b66a6846e727b32acc23d007b71111eaa
Gerodotry <60816964+Gerodotry@users.noreply.github.com>
2023-04-29 21:22:35 +0300

    add inverse and divide cpp
    add cpp files

---
+++ sources/GUI/sources/mod_inverse.cpp
@@ -0,0 +1,35 @@
+#include "mod_inverse.h"
+#include <iostream>
+
+int GCDExtended::calculate(int a, int b, int *x, int *y)
+{
+    if (a == 0)
+    {
+        *x = 0;
+        *y = 1;
+        return b;
+    }
+
+    int x1, y1;
+    int gcd = calculate(b % a, a, &x1, &y1);
+    *x = y1 - (b / a) * x1;
+    *y = x1;
+
+    return gcd;
+}
+
+int ModInverse::calculate(int a, int m)
+{
+    int x, y;
+    int gcd = GCDExtended::calculate(a, m, &x, &y);
+
+    if (gcd != 1)
+    {
+        std::cout << "Inverse does not exist";
+        return -1;
+    }
+    else
+    {
+        return (x % m + m) % m;
+    }
+}
