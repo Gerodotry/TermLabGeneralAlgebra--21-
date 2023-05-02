2b37e311e44f5da52081bd7a0e46e7e5155e9798
RaynGoslenko <olkhovatyi@knu.ua>
2023-05-02 17:31:32 +0300

    Fixed addition

--- sources/algorithms/NumberAddition.cpp
+++ sources/algorithms/NumberAddition.cpp
@@ -7,25 +7,28 @@ Number NumberAddition::run(Number a, Number b, unsigned int modulo) {
 }
 
 Number NumberAddition::add(const Number& a, const Number& b, unsigned int modulo) {
+    unsigned int numeral = modulo ? modulo : 10;
     Number result;
     std::size_t minSize = std::min(a.digits.size(), b.digits.size());
     bool carry = false;
     for (std::size_t i = 0; i < minSize; i++) {
         unsigned int v = a.digits[i] + b.digits[i] + carry;
-        carry = v >= modulo;
-        result.digits.emplace_back(v - modulo * carry);
+        carry = v >= numeral;
+        result.digits.emplace_back(v - numeral * carry);
     }
     Number longer = a.digits.size() > b.digits.size() ? a : b;
     std::size_t i = minSize;
     while (i < longer.digits.size()) {
         unsigned int v = longer[i] + carry;
-        carry = v >= modulo;
-        result.digits.emplace_back(v - modulo * carry);
+        carry = v >= numeral;
+        result.digits.emplace_back(v - numeral * carry);
         i++;
     }
     if (carry) {
         result.digits.emplace_back(1);
     }
-    result.toField(modulo);
+    if (modulo) {
+        result.toField(modulo);
+    }
     return result;
 }
