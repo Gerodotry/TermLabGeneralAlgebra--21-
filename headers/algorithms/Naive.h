8078551a3b51fefc38e07b788d533247c348b124
Radchenko Mukola <RadchenkoMukola@gmail.com>
2023-05-02 17:53:57 +0300

    Added 2 factorization algos need testing

---
+++ headers/algorithms/Naive.h
@@ -0,0 +1,14 @@
+﻿#ifndef LAB_NAIVE_H
+#define LAB_NAIVE_H
+
+#include "utils/Number.h"
+#include <vector>
+
+class Naive {
+public:
+    static vector<Number> run(Number a, unsigned int modulo);
+
+private:
+    static vector<Number> naive_factorization(Number& a,unsigned int modulo);
+
+#endif //LAB_NAIVE_H
