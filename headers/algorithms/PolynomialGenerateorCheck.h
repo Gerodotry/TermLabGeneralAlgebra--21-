73c1c2cd7a0f518d23683b1449a4707f18b3c74a
VladVoloshyn <90627712+VladVoloshyn@users.noreply.github.com>
2023-05-09 00:35:14 +0300

    Polinom

---
+++ headers/algorithms/PolynomialGenerateorCheck.h
@@ -0,0 +1,40 @@
+#ifndef LAB_PolynomialGenerateorCheck_H
+#define LAB_PolynomialGenerateorCheck_H
+#include <vector>
+
+class PolynomialGeneratorCheck {
+public:
+
+    static bool is_generator(const std::vector<int>& poly, int field_size) {
+        std::vector<int> powers(field_size - 1, 0);
+        int x = 1;
+        for (int i = 0; i < field_size - 2; i++) {
+            powers[x] = i + 1;
+            x = multiply_polynomials(x, poly, field_size);
+        }
+
+        for (int i = 1; i < field_size; i++) {
+            if (powers[i] == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+private:
+
+    static std::vector<int> multiply_polynomials(const std::vector<int>& a, const std::vector<int>& b, int field_size) {
+        std::vector<int> result(a.size() + b.size() - 1, 0);
+        for (int i = 0; i < a.size(); i++) {
+            for (int j = 0; j < b.size(); j++) {
+                result[i + j] = (result[i + j] + a[i] * b[j]) % field_size;
+            }
+        }
+
+        while (result.size() > 1 && result.back() == 0) {
+            result.pop_back();
+        }
+        return result;
+    }
+};
+#endif
