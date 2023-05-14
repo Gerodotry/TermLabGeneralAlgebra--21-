f779107633f5dcd95227fd91aaa8e14f25da80a1
KlishchD <klishch.dmytro@gmail.com>
2023-05-14 20:22:10 +0300

    Fixed empty remainder bug

--- headers/algorithms/PolynomialDivision.h
+++ headers/algorithms/PolynomialDivision.h
@@ -43,6 +43,8 @@ T PolynomialDivision::divide(const T& dividend, T divisor, unsigned int modulo) 
             remainder.terms.erase(remainder.terms.begin());
         }
 
+        if (remainder.terms.empty()) break;
+
         Number degree_diff = NumberSubtraction::run(remainder.terms.front().degree, divisor.terms.front().degree, 0);
         Number coeff_ratio = NumberDivision::run(remainder.terms.front().coefficient, divisor.terms.front().coefficient, 0);
 
