3841824b803d5111b5517bad4065936d8eb5bc66
KlishchD <klishch.dmytro@gmail.com>
2023-05-16 22:06:52 +0300

    Moved inverse to long numbers

--- sources/GUI/algorithms/InverseAlgorithm.cpp
+++ sources/GUI/algorithms/InverseAlgorithm.cpp
@@ -15,15 +15,14 @@ std::string InverseAlgorithm::getName() const {
 
 Object *InverseAlgorithm::run() {
     Number *numberA = dynamic_cast<Number *>(dataTypes[0]->getObject());
-    int module = dynamic_cast<Int *>(dataTypes[1]->getObject())->get();
-    result = Inverse::run(*numberA, module);
+    Number *module = dynamic_cast<Number *>(dataTypes[1]->getObject());
+    result = Inverse::run(*numberA, *module);
     return &result;
 }
 
 InverseAlgorithm::InverseAlgorithm() {
     dataTypes = {
             std::make_shared<NumberInputComponent>("NumberA"),
-            std::make_shared<IntInputComponent>("Module")
+            std::make_shared<NumberInputComponent>("Module")
     };
-}// Created by mac on 08.05.2023.
-//
+}
--- sources/algorithms/Inverse.cpp
+++ sources/algorithms/Inverse.cpp
@@ -4,26 +4,26 @@
 // Created by mac on 08.05.2023.
 //
 
-Number Inverse::run(Number a, unsigned int modulo) {
-    if (modulo) {
+Number Inverse::run(Number a, Number modulo) {
+    if (!modulo.isZero()) {
         a.toField(modulo);
     }
     return inverse(a, modulo);
 }
 
-Number Inverse::inverse(Number a, unsigned int modulo) {
-    Number originalM = Number(modulo);
+Number Inverse::inverse(Number& a, Number& modulo) {
+    Number originalM = modulo;
     Number x(0);
     Number y(1);
     Number lastX(1);
     Number lastY(0);
 
 
-    while (modulo != 0) {
-        Number quotient = a / Number(modulo);
+    while (!modulo.isZero()) {
+        Number quotient = a / modulo;
 
-        Number temp = Number(modulo);
-        modulo = (a % Number(modulo)).get();
+        Number temp = modulo;
+        modulo = (a % modulo);
         a = temp;
 
         temp = x;
@@ -34,8 +34,9 @@ Number Inverse::inverse(Number a, unsigned int modulo) {
         y = NumberSubtraction::run(lastY, NumberMultiplication::run(quotient, y, 0), 0);
         lastY = temp;
     }
+
     if (a != 1) {
-        return Number(-1);
+        return Number("-1");
     }
 
     if (!lastX.isPositive) {
