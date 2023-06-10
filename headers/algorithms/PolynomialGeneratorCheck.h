d8dda2392b9ccac99535b69f32149210d3580fca
VladVoloshyn <90627712+VladVoloshyn@users.noreply.github.com>
2023-06-11 00:43:05 +0300

    Fixed bug PolynomialGeneratorCheck.h

--- headers/algorithms/PolynomialGeneratorCheck.h
+++ headers/algorithms/PolynomialGeneratorCheck.h
@@ -8,8 +8,10 @@ public:
     static int find_order(const std::vector<int>& degrees);
 
     static int run(const std::vector<int>& poly, int field_size);
-private:
+
     static bool is_generator(const std::vector<int>& poly, int field_size);
+private:
+    
 
     static std::vector<int> multiply_polynomials(const std::vector<int>& a, const std::vector<int>& b, int field_size);
 
